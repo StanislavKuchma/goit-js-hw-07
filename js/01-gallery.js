c3cc5dc8613818169cdb9633d1baaaa2a9d710bb
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-02-02 22:57:57 +0200

    1task

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+
+const container = document.querySelector(`div[class="gallery"]`);
+
+const imageMarkup = createImageItem(galleryItems);
+
+container.insertAdjacentHTML(`beforeend`, imageMarkup);
+
+container.addEventListener(`click`, onClick);
+
+function createImageItem(item) {
+   return item
+        .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    >
+    </a>
+    </div>`
+        })
+        .join(``);
+ }
+
+function onClick(evt) { 
+     evt.preventDefault();
+    if (!evt.target.classList.contains(`gallery__image`)) {
+        return;
+    }
+
+
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+ }
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,12 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"></link>
+
+    
+    
+    
+ 
+
+<script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>
